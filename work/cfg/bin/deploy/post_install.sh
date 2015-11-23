e68074a68daa7d28d6fa50959792801565e6b29c
Alex Gusev <alex@flancer64.com>
2015-11-23 19:16:26 +0200

    MOBI-106 - Improve live versions for Generics M1 & M2

--- work/cfg/bin/deploy/post_install.sh
+++ work/cfg/bin/deploy/post_install.sh
@@ -52,7 +52,7 @@ fi
 ##
 echo "Drop database '$DB_NAME'."
 ##
-mysqladmin -f -u"$DB_USER" $MYSQL_PASS -h"$DB_HOST" drop if exists "$DB_NAME"
+mysqladmin -f -u"$DB_USER" $MYSQL_PASS -h"$DB_HOST" drop "$DB_NAME"
 mysqladmin -f -u"$DB_USER" $MYSQL_PASS -h"$DB_HOST" create "$DB_NAME"
 
 
